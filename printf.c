91c1a53fb714830b249bf8ea36631b966deeadb9
Mussa Milanzi <viewholder74@gmail.com>
2023-08-21 00:55:00 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -68,6 +68,26 @@ void print_integer(int num, int *count)
 	free(numStr);
 }
 
+/**
+ * print_binary - print in into binary
+ * @num: integer
+ * @count: int count
+ * Return: void
+ */
+void print_binary(unsigned int num, int *count)
+{
+	int i, j = 0;
+	char binaryStr[32];
+
+	while (num > 0)
+	{
+		binaryStr[j++] = '0' + (num & 1);
+		num >>= 1;
+	}
+
+	for (i = j - 1; i >= 0; --i)
+		print_char(binaryStr[i], count);
+}
 
 /**
  * _printf - Function that produces output according to a format.
@@ -83,6 +103,7 @@ int _printf(const char *format, ...)
 	char *str;
 	char c, ch;
 	int i, num, counter = 0;
+	unsigned int b_num;
 
 	va_start(list, format);
 	for (i = 0; (c = format[i]); ++i)
@@ -90,13 +111,15 @@ int _printf(const char *format, ...)
 		if (c == '%')
 		{
 			c = format[++i];
-			if ((c == 'c') && (c == 's') && (c == '%'))
+			if ((c == 'c') && (c == 's') && (c == '%') && (c == 'b'))
 			{
 				ch = va_arg(list, int);
 				str = va_arg(list, char *);
+				b_num = va_arg(list, unsigned int);
 				print_char(ch, &counter);
 				print_string(str, &counter);
 				print_char('%', &counter);
+				print_binary(b_num, &counter);
 			}
 			else if (c == 'd' || c == 'i')
 			{
